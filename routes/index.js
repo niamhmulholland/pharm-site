f28cbe7794dd16bed0438049e6112f487e476add
niamhmulholland <niamhmulholland@college.harvard.edu>
2016-08-26 16:26:42 +0200

    sendgrid test

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+
+router.get('/', function(req, res) {
+	
+	sendgrid.send({
+      to:       'niamhclairemulholland@gmail.com',
+      from:     'noreply@test.com',
+      subject:  'Test',
+      text:     'Welcome'
+    }, function(err, json) {
+      if (err) { return res.send('ahhhh');}    
+      res.send('yayyy');
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
                                
 var express = require('express');
 var app = express();
-var sendgrid = require('sendgrid')(SENDGRID_USERNAME, SENDGRID_PASSWORD);
+
+var routes = require('./routes/index');
 
 // set the port of our application                                              
 // process.env.PORT lets the port be set by Heroku                              
@@ -10,26 +11,18 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs                                                   
 app.set('view engine', 'ejs');
 
+//use routes 
+app.use('/', routes);
+
 // make express look in the public directory for assets (css/js/img)            
 app.use(express.static(__dirname + '/public'));
+
 
 // set the home page route                                                      
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder                       
     res.render('index');
-
-    //email on page load
-
-    sendgrid.send({
-    	to: 'niamhclairemulholland@gmail.com',
-    	from: 'duffyspharmacypage',
-    	subject: 'test',
-    	text: 'hey i think it works'
-    }, function(errk, json) {
-    	if (err) {return console.error(err); }
-    	console.log(json);
-    });
    
 });
 
