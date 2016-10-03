671ca778c4d080c22123bbbd8e90423defe5ae74
niamhmulholland <niamhmulholland@college.harvard.edu>
2016-10-02 20:37:37 -0400

    moved index.js code to server.js

--- index.js
+++ index.js
@@ -6,43 +6,6 @@ var bodyParser = require('body-parser');
 
 console.log('page load');
 
-/*
-
-app.post('photolab-form', function(request, response){  
-
-  console.log('button request');
-
-    //var email = request.query.email;
-    //var message = request.query.message;
-
-    sendgrid.send({
-      to:       'niamhmulholland@college.harvard.edu',
-      from:     'email',
-      subject:  'Test',
-      text:     'message'
-    }, function(err, json) {
-      if (err) { return console.log('error in form submit');}    
-      console.log('form sent');
-      //response.statusCode = 302; 
-      //response.setHeader("Location", "/login");
-      //response.end();
-    }); 
-}); */
-
-/*router.post('photolab-form', function(req, res) {
-
-// http://stackoverflow.com/questions/37200003/how-do-i-handle-a-form-buttons-post-request-in-node-js-and-express
-// stackoverflow example might work? 
-		
-		console.log(request.query.message);
-
-
-
-//alternative 2: make var data and use sendgrid? 
-
-	//var email = request.query.email;
-	//var message = request.query.message;*/
-
 router.get('/', function(req, res) {	
 	sendgrid.send({
       to:       'niamhmulholland@college.harvard.edu',
@@ -50,8 +13,8 @@ router.get('/', function(req, res) {
       subject:  'Test',
       text:     'Welcome'
     }, function(err, json) {
-      if (err) { return console.log('error in form submit');}    
-      console.log('form sent');
+      if (err) { return res.send('error in form submit');}    
+      res.send('fyay');
     }); 
 }); 
 
--- server.js
+++ server.js
@@ -31,6 +31,27 @@ app.listen(port, function() {
 });
 
 
+var express = require('express');
+var app = express();
+var router = express.Router();
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+var bodyParser = require('body-parser');
+
+console.log('page load');
+
+router.get('/', function(req, res) {	
+	sendgrid.send({
+      to:       'niamhmulholland@college.harvard.edu',
+      from:     'noreply@test.com', 
+      subject:  'Test',
+      text:     'Welcome'
+    }, function(err, json) {
+      if (err) { return res.send('error in form submit');}    
+      res.send('fyay');
+    }); 
+}); 
+
+module.exports = router;
 
 
 
